31241a4869db6b30f1beba7362221fa379437336
Stoian02 <stoianstoikov@gmail.com>
2024-10-19 16:57:54 +0300

    Add: flags for Ctrl + keyboard button press

--- ccode.c
+++ ccode.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -15,7 +17,9 @@ void enableRawMode()
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -26,6 +30,11 @@ int main()
 
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-        ;
+    {
+        if (iscntrl(c))
+            printf("%d\r\n", c);
+        else
+            printf("%d ('%c')\r\n", c, c);
+    }
     return 0;
 }
