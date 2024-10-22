f8b11d1fef08600be504ccbb30e453b3f0dc7a0c
Stoian02 <stoianstoikov@gmail.com>
2024-10-22 11:10:44 +0300

    Add: mapped ctrl+q for quit, refactor keyboard input

--- ccode.c
+++ ccode.c
@@ -9,6 +9,11 @@
 #include <unistd.h>
 
 /**
+ * Defines:
+ */
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/**
  * Data:
  */
 struct termios orig_termios;
@@ -47,6 +52,34 @@ void enableRawMode()
 		die("tcsetattr");
 }
 
+char editorReadKey()
+{
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN)
+			die("read");
+	}
+	return c;
+}
+
+/**
+ * Input
+ */
+
+void editorProcessKeypress()
+{
+	char c = editorReadKey();
+
+	switch (c)
+	{
+	case CTRL_KEY('q'):
+		exit(0);
+		break;
+	}
+}
+
 /**
  * Init
  */
@@ -56,18 +89,7 @@ int main()
 
 	while (1)
 	{
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-			die("read");
-
-		read(STDIN_FILENO, &c, 1);
-		if (iscntrl(c))
-			printf("%d\r\n", c);
-		else
-			printf("%d ('%c')\r\n", c, c);
-
-		if (c == 'q')
-			break;
+		editorProcessKeypress();
 	}
 	return 0;
 }
