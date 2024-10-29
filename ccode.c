4fad119e57e27776a10437b06e6c19e9f7be21fd
Stoian02 <stoianstoikov@gmail.com>
2024-10-29 12:40:34 +0200

    Add: delete key (does nothing for now)

--- ccode.c
+++ ccode.c
@@ -30,6 +30,7 @@ enum editorKey
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DELETE_KEY,
     HOME_KEY,
     END_KEY,
     PAGE_UP,
@@ -111,6 +112,8 @@ int editorReadKey()
                     {
                     case '1':
                         return HOME_KEY;
+                    case '3':
+                        return DELETE_KEY;
                     case '4':
                         return END_KEY;
                     case '5':
